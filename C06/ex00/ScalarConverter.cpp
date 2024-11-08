8307ba4450b225433a77ff1bb93341f46c4079fb
jeza <jezaguerin.pro@gmail.com>
2024-11-08 19:50:40 +0100

    Still ex00

--- C06/ex00/ScalarConverter.cpp
+++ C06/ex00/ScalarConverter.cpp
@@ -19,7 +19,7 @@ static void    ScalarConverter::convert(const std::string& literal) {
     if (literal.length() == 3 && literal[0] == '\'' && literal[2] == '\'') {
 
         charValue = literal[1];
-        if (!isprint(charValue) {
+        if (!isprint(charValue)) {
             std::cout << "This is not a printable character !" << std::endl;
             return; // Check if we return or do something.
         }
@@ -34,25 +34,34 @@ static void    ScalarConverter::convert(const std::string& literal) {
             std::cout << "double: " << doubleValue;
         }
     }
+    else {
+        char *endPtr;
+        doubleValue = strtod(literal.c_str(), &endPtr); // transformer une string, représentant textuellement un nb floattant en double.
+        if (*endPtr == 'f' && *(endPtr + 1) == '\0') // Check si float
+        else if (*endPtr == '\0') { // Check si le littéral a bien été converted.
+            if (doubleValue < std::numeric_limits<int>::min() || doubleValue > std::numeric_limits<int>::max()) {  // Check if we check min/max for double !!
+                std::cout << "int: literal is out of range !" << std::endl;
+                return;
+            }
+            else {
+                integerValue = static_cast<int>(doubleValue);
+                std::cout << "int: " << integerValue << std::endl;
 
-    if (literal[0] == '+' || literal[0] == '-')
-        index = 1;
-    for (int i = index; i < literal.length() - 1; i++) {
-        if (!std::isdigit(literal[i]))
-            return ;
-        // If it's a digit convert it to an int.
-    }
-    double  doubleValue = strtod(literal.c_str(), &endPtr);
-    if (*endPtr == '\0') {
-        if (doubleValue < < std::numeric_limits<int>::min() || doubleValue > std::numeric_limits<int>::max()) {  // Check if we check min/max for double !!
-            std::cout << "int: impossible" << std::endl;
-            return;
+                charValue = static_cast<char>(integerValue);
+                if (isprint(charValue))
+                    std::cout << "char: " << charValue;
+                else
+                    std::cerr << "This is not a printable character !" << std::endl;
+
+                floatValue = static_cast<float>(integerValue);
+                std::cout << "float: " << floatValue << std::endl;
+
+                doubleValue = static_cast<double>(integerValue);
+                std::cout << "double: " << doubleValue << std::endl;
+            }
+        }
+        else {
+            std::cerr << "This is not an integer or a char value !" << std::endl;
         }
     }
-    // long long int value = std::strtol(literal.c_str(), nullptr, 10);
-    //     if (value < std::numeric_limits<int>::min() || value > std::numeric_limits<int>::max()) {
-    //         std::cout << "int: impossible" << std::endl;
-    //         return;
-    //     }
-    //     int intValue = static_cast<int>(value);
 }
