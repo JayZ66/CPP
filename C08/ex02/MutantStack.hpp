0a8e7bfc4d666d94bd008c994befee74ea7d216c
jeza <jezaguerin.pro@gmail.com>
2024-11-27 20:47:54 +0100

    Starting ex02 - CPP08

--- C08/ex02/MutantStack.hpp
+++ C08/ex02/MutantStack.hpp
@@ -1,7 +1,26 @@
 
 #pragma once
 
-class   MutantStack {
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <time.h>
+#include <ctime>
+#include <cstdlib>
+#include <stack>
+
+// Stack is a container that follows LIFO principle (Last in, First out)
+// By default it is using a std::deque as an underlying container (sous-jacent) but we can use others like vector, list, etc.
+
+// Key features : no index access + specify the container type when we declare std::stack
+
+template <typename T>
+class   MutantStack : public std::stack<T> {
     private:
 
     public:
@@ -11,4 +30,15 @@ class   MutantStack {
     ~MutantStack();
 
     MutantStack& operator=(const MutantStack& other);
-}
+
+    template <typename T>
+    typename T::iterator begin();
+
+    template <typename T>
+    typename T::iteraot end();
+};
+
+/*
+Principales méthodes de std::stack : 
+
+*/
